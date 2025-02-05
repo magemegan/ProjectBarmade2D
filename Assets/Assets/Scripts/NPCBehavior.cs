cd1d4c535251b3d0620078487cd4a6db471dfa57
DrLivingSteak <cowbiscuit2@gmail.com>
2025-02-05 14:30:30 -0500

    Added Basic Pathing to ONE object

--- Assets/Assets/Scripts/NPCBehavior.cs
+++ Assets/Assets/Scripts/NPCBehavior.cs
@@ -4,35 +4,48 @@ using UnityEngine;
 
 public class NPCBehavior : MonoBehaviour
 {
+    public bool xHigher;
+    public bool yHigher;
     // Start is called before the first frame update
     void Start()
     {
-        
+        // QualitySettings.vSyncCount = 0;
+        // Application.targetFrameRate = 60;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 position = transform.position;
-        Vector2 position2;
-        position2.x = -6;
-        position2.y = 0;
+        Vector2 pointPos = GameObject.Find("NpcPoint2").transform.position; //Finds a designated GameObject. Will be changed later, since we need to find multiple objects
+        Vector2 position = transform.position; // NPC's position
+        if (Mathf.Round(position.x) != Mathf.Round(pointPos.x) && yHigher == false){
+            xHigher = true;
+        }
+        else if (Mathf.Round(position.y) != Mathf.Round(pointPos.y) && xHigher == false){
+            yHigher = true;
+        }
+        else if (Mathf.Round(position.x) == Mathf.Round(pointPos.x)){
+            xHigher = false;
+        }
+        else if (Mathf.Round(position.y) == Mathf.Round(pointPos.y)){
+            yHigher = false;
+        }
 
-        if (position.x > position2.x){
-            position.x = position.x - 0.01f;
+        if (xHigher == true){
+            if (position.x > pointPos.x){
+                position.x = position.x - 0.01f;
+            }
+            else{
+                position.x = position.x + 0.01f;
+            }
         }
-        
-        if (position.x < position2.x){
-            position.x = position.x + 0.01f;
-        }
-
-        
-        if (position.y > position2.y){
-             position.y = position.y - 0.01f;
-        }
-        
-        if (position.y < position2.y){
-            position.y = position.y + 0.01f;
+        if (yHigher == true){
+            if (position.y > pointPos.y){
+                position.y = position.y - 0.01f;
+            }
+            else{
+                position.y = position.y + 0.01f;
+            }      
         }
         transform.position = position;
     }
