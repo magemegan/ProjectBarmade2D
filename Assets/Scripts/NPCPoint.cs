5e4a7b3659111ea7bb43580ba127f2efcbc41c60
DrLivingSteak <cowbiscuit2@gmail.com>
2025-02-12 14:55:51 -0500

    NPC Changes (Spawn interval added)

--- Assets/Scripts/NPCPoint.cs
+++ Assets/Scripts/NPCPoint.cs
@@ -2,29 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+//READ THIS: MOST, IF NOT ALL OF THE PUBLIC VARIABLES ARE BEING ASSIGNED TO THE GREEN CIRCLE NPCPOINT!
+
+
 public class NPCObjects : MonoBehaviour
 {
     public GameObject NPC;
+    private bool spawnNPC;
+    public int startTime;
+    public int changeInterval1;
+    public int changeInterval2;
+    bool occupied = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnNPC = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)){
-
+        if (Mathf.Round(Time.time) == startTime && spawnNPC == true && occupied == false){
+        
         // Generate a random position on the screen
 
-        Vector2 spawnPosition = new Vector2(Random.Range(-5f, 5f), Random.Range(-5f, 5f));
+        Vector2 spawnPosition = new Vector2(0, 4);
 
 
 
         // Instantiate the enemy at the spawn position
 
         Instantiate(NPC, spawnPosition, Quaternion.identity);
+
+        startTime = startTime + Random.Range(changeInterval1, changeInterval2);
         }
+        Debug.Log("Current in-game time: " + Mathf.Round(Time.time)); 
+        
     }
 }
