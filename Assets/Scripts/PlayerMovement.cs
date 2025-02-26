d48213838195151da3b07182f9dd85e0be41b5f9
skatermage <MEGANCM9@AOL.COM>
2025-02-26 14:17:23 -0500

    Interact plus NPC

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private PickUp pickUp;
     public float moveSpeed;
     Rigidbody2D rb;
     private Vector2 movement;
@@ -11,21 +12,35 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Interactable setup
+        pickUp = gameObject.GetComponent<PickUp>();
+        //pickUp.Direction = new Vector2(0, -1);
         rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Get keyboard input
+        // Get keyboard input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+        // Discover what the player is looking at
+        if (movement.sqrMagnitude > .1f)
+        {
+            //pickUp.Direction = movement.normalized;
+        }
 
-        movement = movement.normalized;//Normalize movement to prevent faster diagonal movement
+        movement = movement.normalized;// Normalize movement to prevent faster diagonal movement
+
+        
+
     }
+
+    
 
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
+
