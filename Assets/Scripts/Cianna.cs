b9bf43f872f214e229172fef87b6d51c93f4e8cb
purechaosss <ciannanorris55@gmail.com>
2025-03-05 16:31:46 -0500

    Updates

--- Assets/Scripts/Cianna.cs
+++ Assets/Scripts/Cianna.cs
@@ -33,17 +33,17 @@ public class DraftBeerMachine : MonoBehaviour
         beerSelectionUI.SetActive(true);
     }
 
-    private void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             isPlayerNear = true;
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnCollisionExit2D(Collision2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player"))
         {
             isPlayerNear = false;
             beerSelectionUI.SetActive(false);
