137fec59beb9176229c1a4c8338e7b421243b247
skatermage <MEGANCM9@AOL.COM>
2025-04-09 15:52:11 -0400

    toxic bar mechanics finished
    - just need art implemented

--- Assets/Scripts/CollidableObjects.cs
+++ Assets/Scripts/CollidableObjects.cs
@@ -5,29 +5,22 @@ using UnityEngine;
 public class CollidableObjects : MonoBehaviour
 {
     private Collider2D z_Collider;
-    [SerializeField]
     private ContactFilter2D z_Filter;
-    private List<Collider2D> z_CollidedObjects = new List<Collider2D>(1);
+    private List<Collider2D> z_CollidedObjects;
 
-    protected virtual void Start()
+    private void Start()
     {
         z_Collider = GetComponent<Collider2D>();
 
     }
 
-    protected virtual void Update()
+    private void Update()
     {
         z_Collider.OverlapCollider(z_Filter, z_CollidedObjects);
-        foreach (var o in z_CollidedObjects)
+        foreach(var o in z_CollidedObjects)
         {
-
-            OnCollided(o.gameObject);
+            Debug.Log("Colliede with " + o.name);
         }
-    }
-
-    protected virtual void OnCollided(GameObject collidedObject)
-    {
-        Debug.Log("Colliede with " + collidedObject.name);
     }
 
 }
--- Assets/Scripts/NPCIteract.cs
+++ Assets/Scripts/NPCIteract.cs
@@ -2,14 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NPCIteract : PickUp
+public class NPCIteract : MonoBehaviour
 {
+    public float maxDrunk = 100;
+    public float currentDrunkness = 0;
+    public bool addedDrink = false;
+    public ToxicBar toxicBar;
+    public float NPCtolerance;
+    // value from 0 - 1
+    
 
 
-    //NPCIteract(int toxicMeter)
-    //{
-    //    PickUp newNPC = new PickUp();
-    //    toxicMeter = newNPC.getToxicMeter();
-    //    this.toxicMeter = toxicMeter;
-    //}
+    private void Start()
+    {
+        toxicBar.SetDrunkness(currentDrunkness);
+
+        toxicBar.SetMaxDrunkness(50);
+    }
+
+
+    private void Update()
+    {
+       
+
+    }
+
+    public void AddDrink(int drunk)
+    {
+        //addedDrink = true;
+        //currentDrunkness += drunk;
+        float initialToxic = Random.Range(5, drunk);
+        float reduceIntoxication = initialToxic * NPCtolerance;
+        float finalIntoxication = initialToxic - reduceIntoxication;
+        
+        currentDrunkness = Mathf.Clamp(currentDrunkness + finalIntoxication, 0, maxDrunk);
+
+        toxicBar.SetDrunkness(currentDrunkness);
+    }
+
 }
