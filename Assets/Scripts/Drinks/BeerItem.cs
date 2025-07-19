0e527a13cef803d77382e00533f2e4602128de7d
Danae Troupe <danaetroupe@gmail.com>
2025-07-19 08:29:07 -0400

    Removing unnecessary comments

--- Assets/Scripts/Drinks/BeerItem.cs
+++ Assets/Scripts/Drinks/BeerItem.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class BeerItem : DrinkController
 {
-    //public Sprite displayImage; Might bring this back later :/
-    // Start is called before the first frame update
-
     public void SayName()
     {
         Debug.Log(gameObject.name);
