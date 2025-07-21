a1fc4a055374c15876805cb88512efc4fb635edd
Danae Troupe <danaetroupe@gmail.com>
2025-07-21 19:42:51 -0400

    Refactoring tests

--- Assets/Tests/EditMode/NPC/NPCOrderingTests.cs
+++ Assets/Tests/EditMode/NPC/NPCOrderingTests.cs
@@ -6,12 +6,35 @@ using UnityEngine.TestTools;
 
 public class NPCOrderingTests
 {
+    private NPCOrdering npcOrdering;
+    private List<GameObject> testObjects;
+
+    [SetUp]
+    public void Setup()
+    {
+        testObjects = new List<GameObject>();
+        GameObject testObject = new GameObject("TestNPCOrdering");
+        testObjects.Add(testObject);
+        npcOrdering = testObject.AddComponent<NPCOrdering>();
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        foreach (GameObject obj in testObjects)
+        {
+            if (obj != null)
+            {
+                Object.DestroyImmediate(obj);
+            }
+        }
+        testObjects.Clear();
+    }
+
     [Test]
     public void GetRandomRecipe_ReturnsRecipe()
     {
-        NPCOrdering ordering = new NPCOrdering();
-        Recipe recipe = ordering.GetRandomRecipe();
-        
+        Recipe recipe = npcOrdering.GetRandomRecipe();
 
         Assert.IsNotNull(recipe);
     }
@@ -19,10 +42,22 @@ public class NPCOrderingTests
     [Test]
     public void GetRandomRecipe_RecipeIsUnlocked()
     {
-        NPCOrdering ordering = new NPCOrdering();
-        Recipe randomRecipe = ordering.GetRandomRecipe();
+        Recipe randomRecipe = npcOrdering.GetRandomRecipe();
         bool isUnlocked = randomRecipe.getUnlocked();
-
         Assert.True(isUnlocked);
+    }
+
+    [Test]
+    public void GetRecipeAccuracy_ReturnsZero()
+    {
+        Recipe randomRecipe = npcOrdering.GetRandomRecipe();
+
+        GameObject drinkObject = new GameObject("TestDrink");
+        testObjects.Add(drinkObject);
+        DrinkController emptyDrink = drinkObject.AddComponent<DrinkController>();
+
+        float accuracy = npcOrdering.GetRecipeAccuracy(randomRecipe, emptyDrink);
+
+        Assert.AreEqual(0, accuracy);
     }
 }
